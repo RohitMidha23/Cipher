829a587289f987276a248c4c11abe68710d2c4d9
RohitMidha23 <38888530+RohitMidha23@users.noreply.github.com>
2020-09-17 06:17:03 +0000

    comment fix

--- rowcolumn.java
+++ rowcolumn.java
@@ -72,7 +72,7 @@ public class rowcolumn {
         }
         int index=0;
         for(int i=0; i<keyLength; i++){
-            // key[i] is column index
+            // map[i] is column index
             // j is row index
             for(int j=0; j<rows; j++){
                 matrix[j][map.get(i)] = text.charAt(index++);
