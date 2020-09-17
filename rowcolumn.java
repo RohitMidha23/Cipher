506aaa9302f049c9de13252058acb1cc7e4ecb54
RohitMidha23 <38888530+RohitMidha23@users.noreply.github.com>
2020-09-17 04:42:22 +0000

    row column

---
+++ rowcolumn.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class rowcolumn {
+
+    public static void printMatrix(char[][] matrix, int row, int column){
+        char ignore='1';
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                if(matrix[i][j] == ignore)
+                    System.out.print(" " + "\t");
+                else
+                    System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static String encrypt(String text, int[] key){
+        String encrypted = "";
+        int keyLength = key.length;
+        int columns = keyLength;
+        int rows = (int) (text.length() / columns);
+        char[][] matrix = new char[rows][columns];
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+               matrix[i][j] = '1'; 
+            }
+        }
+        int index=0; boolean flag = false;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                if(index<text.length())
+                    matrix[i][j] = text.charAt(index++);
+                else{
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag)
+                break;
+        }
+        System.out.println("ENCRYPTION MATRIX:");
+        printMatrix(matrix, rows, columns);
+        for(int i=0; i<keyLength; i++){
+            // key[i] is column index
+            // j is row index
+            for(int j=0; j<rows; j++){
+                encrypted += matrix[j][key[i]];
+            }
+        }
+        return encrypted;
+    }
+
+    public static String decrypt(String text, int[] key){
+        String decrypted = "";
+        int keyLength = key.length;
+        int columns = keyLength;
+        int rows = (int) (text.length() / columns);
+        char[][] matrix = new char[rows][columns];
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+               matrix[i][j] = '1'; 
+            }
+        }
+        int index=0;
+        for(int i=0; i<keyLength; i++){
+            // key[i] is column index
+            // j is row index
+            for(int j=0; j<rows; j++){
+                matrix[j][key[i]] = text.charAt(index++);
+            }
+        }
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+               decrypted += matrix[i][j];
+            }
+        }
+
+        return decrypted;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter text to encrypt:");
+        String text = s.next();
+        System.out.print("Enter key length:");
+        int n; n = s.nextInt();
+        while(text.length() % n !=0){
+            System.out.println("Key Length should divide text length!");
+            System.out.print("Enter key length:");
+            n = s.nextInt();
+        }
+        System.out.println("Enter key (from 1 to " + n + "):");
+        int[] key = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            key[i] = s.nextInt() - 1; // -1 for zero indexing
+        }
+    
+        String encrypted = encrypt(text, key);
+        System.out.println("ENCRYPTED : " + encrypted); 
+    
+        String decrypted = decrypt(encrypted, key);
+        System.out.println("DECRYPTED : " + decrypted); 
+        
+    }
+}
